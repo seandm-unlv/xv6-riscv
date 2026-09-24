96df738004478bd97f8343a4d724b7b84ede8a23
seandm-unlv <milans1@unlv.nevada.edu>
2026-09-24 00:01:32 -0700

    Update defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -60,7 +60,6 @@ void            ireclaim(int);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-int             syscalltrace(void);
 
 // log.c
 void            initlog(int, struct superblock*);
